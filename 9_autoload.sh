b1e77a4e8db370bba2d63499d09aebc310550dcc
Mehdi Lebdi <mlebdi411@gmail.com>
2020-02-20 17:20:56 -0800

    Update 9_autoload.sh

--- 9_autoload.sh
+++ 9_autoload.sh
@@ -3,6 +3,6 @@
 
 while true
 do
-	../Software/mongodb-osx-x86_64-4.0.4/bin/mongoexport --host cmpt733-shard-0/cmpt733-shard-00-00-stzkw.mongodb.net:27017,cmpt733-shard-00-01-stzkw.mongodb.net:27017,cmpt733-shard-00-02-stzkw.mongodb.net:27017 --ssl --db iot_prediction --collection battery --username falcon --password vancouver --authenticationDatabase admin --type=csv --fields="Battery_Type,Battery_Level,DateTime,User_Type,Latitude,Battery_Cycle_No,Location,u_id,Langitude,Battery_Status,full_name" --out ../Tableau_iot_data/final.csv
+	../Software/mongodb-osx-x86_64-4.0.4/bin/mongoexport --host cmpt733-shard-0/cmpt733-shard-00-00-stzkw.mongodb.net:27017,cmpt733-shard-00-01-stzkw.mongodb.net:27017,cmpt733-shard-00-02-stzkw.mongodb.net:27017 --ssl --db iot_prediction --collection battery --username ***** --password ****** --authenticationDatabase admin --type=csv --fields="Battery_Type,Battery_Level,DateTime,User_Type,Latitude,Battery_Cycle_No,Location,u_id,Langitude,Battery_Status,full_name" --out ../Tableau_iot_data/final.csv
 	sleep 5
 done
